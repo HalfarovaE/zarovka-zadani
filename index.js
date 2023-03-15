2bb867775b76ec0703baa7151286f18f095c68f4
Eva Halfarova <halfarovaevacz@gmail.com>
2023-03-15 18:52:45 +0100

    verze 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+const changeBulb =()=>{
+
+const bulbOff = document.querySelector(".bulb")
+bulbOff.classList.toggle("bulb--on")
+
+
+}
+
+
+document.addEventListener("keydown", changeBulb)
+
